47ef2a46553ddc821e05c302e2c3a8f17a4251da
Femilya Juniar <femilyaniar222@gmail.com>
2020-11-10 09:03:01 +0700

    inisialisasi pertamakali

---
+++ app/src/main/java/com/azhar/deliserdang/api/Api.java
@@ -0,0 +1,16 @@
+package com.azhar.deliserdang.api;
+
+public class Api {
+
+    public static String Hotel = "https://dev.farizdotid.com/api/purwakarta/hotel";
+    public static String Kuliner = "https://dev.farizdotid.com/api/purwakarta/kuliner";
+    public static String DetailKuliner = "https://dev.farizdotid.com/api/purwakarta/kuliner/{id}";
+    public static String TempatIbadah = "https://dev.farizdotid.com/api/purwakarta/tempatibadah";
+    public static String Wisata = "https://dev.farizdotid.com/api/purwakarta/wisata";
+    public static String DetailWisata = "https://dev.farizdotid.com/api/purwakarta/wisata/{id}";
+    /*public static String Komunitas = "https://dev.farizdotid.com/api/purwakarta/komunitas";
+    public static String DetailKomunitas = "https://covid19.deliserdangkab.go.id/api/v1/{route}";
+    public static String KategoriKomunitas = "https://dev.farizdotid.com/api/purwakarta/komunitas?kategori={nama_kategori}";
+    public static String RuteAngkot = "https://dev.farizdotid.com/api/purwakarta/ruteangkot";*/
+
+}
